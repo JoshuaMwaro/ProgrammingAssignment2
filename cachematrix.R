2ca5461fb3fb260be84acf24e3a075b61e7e0303
JoshuaMwaro <104411533+JoshuaMwaro@users.noreply.github.com>
2022-10-25 10:52:45 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## The function below ("makeCacheMatrix") creates a special “matrix”, 
 ## Within the special "matrix" function, is a list containing a function to:
 # set the value of the matrix
